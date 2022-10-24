d10f1048691be1317d34431ddf1b4ae16c7547a6
Isa Tippens <isatippens2@gmail.com>
2022-10-24 21:03:24 +0200

    Handle network api errors

--- client/src/api/mysqlapi.js
+++ client/src/api/mysqlapi.js
@@ -3,9 +3,28 @@ import axios from "axios"
 const URL = process.env.REACT_APP_SPACEFORCE_URL || "http://localhost:3002"
 
 export const get = async(endpoint) => {
-    return axios.get(URL + endpoint)
+    let response = await axios.get(URL + endpoint)
+    if (response.status != 200) 
+        return {
+            sucess: false,
+            data: []
+        }
+    return {
+        success: true,
+        data: response.data
+    }
 }
 
 export const post = async(endpoint, data) => {
-    return axios.post(URL + endpoint, data) ;
+    let response = axios.post(URL + endpoint, data) ;
+    if (response.status != 200) {
+        return {
+            sucess: false,
+            data: []
+        }
+    }
+    return {
+        success: true,
+        data: response.data
+    }
 }
