c69f3aef404f2c8d2c29465c1b09c734b6b4274c
Isa Tippens <isatippens2@gmail.com>
2022-10-24 20:17:30 +0200

    Fix api inputs

--- client/src/api/passenger.js
+++ client/src/api/passenger.js
@@ -2,12 +2,16 @@ import {get, post} from './mysqlapi';
 
 const ENDPOINT = "/api/passenger"
 
-export const getPassenger = async (id) => {
-    return await get(ENDPOINT + `/{id}`)
-}
-
 export const getPassengers = async() => {
     return await get(ENDPOINT)
+}
+
+export const getPassenger = async (id) => {
+    return await get(ENDPOINT + `/${id}`)
+}
+
+export const getPassengerByPassport = async (passport) => {
+    return await get(ENDPOINT + `/passport/${passport}`)
 }
 
 export const createPassenger = async(passengerName, passportNum) => {
