18b39f100ac613d6315bdbc0e4213fdedb3f81f8
Isa Tippens <isatippens2@gmail.com>
2022-10-26 01:49:15 +0200

    Fix location api

--- client/src/api/location.js
+++ client/src/api/location.js
@@ -1,8 +1,13 @@
 import {get, post} from './mysqlapi';
 
-const ENDPOINT = '/api/location'
+const ENDPOINT = '/api/location';
+
 export const getLocation = async(id) => {
     return await get(ENDPOINT + `/${id}`)
+}
+
+export const getLocationByName = async(name) => {
+    return await get(ENDPOINT + `/name/${name}`)
 }
 
 export const getLocations = async() => {
