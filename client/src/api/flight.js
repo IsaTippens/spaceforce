b270b9e01d985c7869d5c76bc80f0ec61fcd1b86
Isa Tippens <isatippens2@gmail.com>
2022-10-26 01:49:26 +0200

    Fix fligh api

--- client/src/api/flight.js
+++ client/src/api/flight.js
@@ -1,15 +1,20 @@
 import {get, post} from './mysqlapi';
 
-ENDPOINT = "/api/flight"
-const getFlight= async (id) => {
-    return await get(ENDPOINT + `/{id}`)
+const ENDPOINT = "/api/flight";
+
+export const getFlight= async (id) => {
+    return await get(ENDPOINT + `/${id}`)
 } 
 
-const getFlights = async () => {
+export const getFlightDetails = async (date, deptid, destid, shipid, carrid) => {
+    return await get(ENDPOINT + `/${date}/${deptid}/${destid}/${shipid}/${carrid}`)
+} 
+
+export const getFlights = async () => {
     return await get(ENDPOINT)
 } 
 
-const createFlight = async(departureTime, departureLocationID, destinationID, shipID, carrierID) => {
+export const createFlight = async(departureTime, departureLocationID, destinationID, shipID, carrierID) => {
     return await post(ENDPOINT + "/create", {
         shipID,
         carrierID,
